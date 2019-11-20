4d4a32e1e5d7057e61269b7d813aca4fd1cf0cf7
Student <example@example.com>
2019-11-20 10:56:02 -0800

    add moment, timestamps to tickets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import Header from "./Header";
-import TicketList from "./TicketList";
-import NewTicketControl from "./NewTicketControl";
-import Error404 from "./Error404";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
+import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
 import Moment from 'moment';
 
 class App extends React.Component {
@@ -17,28 +17,49 @@ class App extends React.Component {
   }
   
   componentDidMount() {
+    console.log('componentDidMount');
     this.waitTimeUpdateTimer = setInterval(() =>
       this.updateTicketElapsedWaitTime(),
-      5000
+    60000
     );
   }
   
   componentWillUnmount(){
+    console.log('componentWillUnmount');
     clearInterval(this.waitTimeUpdateTimer);
   }
   
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+
+  componentWillReceiveProps() {
+    console.log('componentWillReceiveProps');
+  }
+
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate');
+    return true;
+  }
+  componentWillUpdate() {
+    console.log('componentWillUpdate');
+  }
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+  
   updateTicketElapsedWaitTime() {
-    console.log("check");
+  
     let newMasterTicketList = this.state.masterTicketList.slice();
     newMasterTicketList.forEach((ticket) =>
-    ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+      ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
     );
-    this.setState({masterTicketList: newMasterTicketList})
+    this.setState({masterTicketList: newMasterTicketList});
   }
   
   handleAddingNewTicketToList(newTicket){
     var newMasterTicketList = this.state.masterTicketList.slice();
-    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true)
+    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true);
     newMasterTicketList.push(newTicket);
     this.setState({masterTicketList: newMasterTicketList});
   }
