e4a7396bcbcd6f7c2cd18c9e3f5257e11f779862
Student <example@example.com>
2019-11-20 10:14:56 -0800

    update lifecycle state methods for ticket time open

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import TicketList from "./TicketList";
 import NewTicketControl from "./NewTicketControl";
 import Error404 from "./Error404";
 import { Switch, Route } from "react-router-dom";
+import Moment from 'moment';
 
 class App extends React.Component {
 
@@ -14,9 +15,30 @@ class App extends React.Component {
     };
     this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
   }
-
+  
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateTicketElapsedWaitTime(),
+      5000
+    );
+  }
+  
+  componentWillUnmount(){
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+  
+  updateTicketElapsedWaitTime() {
+    console.log("check");
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.forEach((ticket) =>
+    ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+    );
+    this.setState({masterTicketList: newMasterTicketList})
+  }
+  
   handleAddingNewTicketToList(newTicket){
     var newMasterTicketList = this.state.masterTicketList.slice();
+    newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true)
     newMasterTicketList.push(newTicket);
     this.setState({masterTicketList: newMasterTicketList});
   }
@@ -28,6 +50,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
           <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          
           <Route component={Error404} />
         </Switch>
       </div>
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+
 function Ticket(props){
   return (
     <div>
       <h3>{props.location} - {props.names}</h3>
+      <h4>{props.formattedWaitTime}</h4>
       <p><em>{props.issue}</em></p>
       <hr/>
     </div>
   );
 }
 
+
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  issue: PropTypes.string
+  issue: PropTypes.string,
+  formattedWaitTime: PropTypes.string.isRequired
 };
 
 export default Ticket;
